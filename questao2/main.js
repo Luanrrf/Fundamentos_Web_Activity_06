116c911d17727e04b5d63acde825171c677bda7c
luanrrf <luanrrf@yahoo.com.br>
2022-01-23 14:01:17 -0300

    correção de erro na questao 2

--- questao2/main.js
+++ questao2/main.js
@@ -3,5 +3,5 @@ var result;
 
 for (let i = 1; i <= 30; i++){
     result = Math.pow(number,i);
-    console.log(number + " elevado a " + i + ":" + " " + result);
+    window.alert(number + " elevado a " + i + ":" + " " + result);
 }
